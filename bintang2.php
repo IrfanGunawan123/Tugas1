09a20ed899dcb92f55879a867fe9cf5768e1352e
IrfanGunawan123 <94823916+IrfanGunawan123@users.noreply.github.com>
2021-11-30 08:00:33 +0700

    Add files via upload

---
+++ bintang2.php
@@ -0,0 +1,11 @@
+<?php
+	for($i=1;$i<=5;$i++){
+	for($b=5; $b>=$i; $b--){
+		echo " &nbsp";
+	}
+	for($a=1;$a<=$i;$a++){
+		echo"*";
+	}
+	echo"<br>";
+	}
+?>
---
+++ bintang3.php
@@ -0,0 +1,11 @@
+<?php
+	for($i=1;$i<=5;$i++){
+	for($b=1; $b<=$i; $b++){
+		echo " &nbsp";
+	}
+	for($a=5;$a>=$i;$a--){
+		echo"*";
+	}
+	echo"<br>";
+	}
+?>
